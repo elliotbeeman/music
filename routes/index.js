669a473ea30595376cef234779dbb4a065fad641
elliotbeeman <elliotbeeman@gmail.com>
2015-02-21 13:40:55 -0500

    First commit?

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/fuckyou', function(req, res, next) {
+  res.render('fuckyou', { title: 'I Fucking Hate You' });
+
+
+});
+
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+module.exports = router;
